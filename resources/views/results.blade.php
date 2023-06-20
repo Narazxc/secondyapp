5d8df8fa1d98125ea38f1c2db5e07097a0bb942a
Sitha Sovannara <sithasovannara51@gmail.com>
2023-06-20 17:38:00 +0700

    Added if/else in result blade to check whether there are products return from filter query

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -17,25 +17,32 @@
 
                 
         <!-- More products... -->
-        @foreach($products as $product)
-        <a href="{{ route('products.show', $product) }}" class="group">
-            <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-            <img src="@if($product->images->first() === null) https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg  @else {{ asset('images/' . $product->images->first()->image_path) }} @endif" alt="Product Image" class="h-full w-full object-cover object-center group-hover:opacity-75">
-            </div>
-            <h3 class="mt-4 text-sm text-gray-700">{{ $product['title'] }}</h3>
-            <div class="my-2">
-                @php
-                    $tags = explode(',', $product->tags);
-                @endphp
+        @if($products->count() === 0)
+            <p>No result found</p>
+        @else
+        
+            @foreach($products as $product)
+            <a href="{{ route('products.show', $product) }}" class="group">
+                <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <img src="@if($product->images->first() === null) https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg  @else {{ asset('images/' . $product->images->first()->image_path) }} @endif" alt="Product Image" class="h-full w-full object-cover object-center group-hover:opacity-75">
+                </div>
+                <h3 class="mt-4 text-sm text-gray-700">{{ $product['title'] }}</h3>
+                <div class="my-2">
+                    @php
+                        $tags = explode(',', $product->tags);
+                    @endphp
 
-                @foreach($tags as $tag)
-                <span class="bg-pink-100 cursor-pointer text-pink-800 text-xs font-medium mr-1 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">{{ $tag }}</span>
-                @endforeach
-            </div>
-            
-            <p class="mt-1 text-lg font-medium text-gray-900">${{ $product['price'] }}</p>
-        </a>
-        @endforeach
+                    @foreach($tags as $tag)
+                    <span class="bg-pink-100 cursor-pointer text-pink-800 text-xs font-medium mr-1 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">{{ $tag }}</span>
+                    @endforeach
+                </div>
+                
+                <p class="mt-1 text-lg font-medium text-gray-900">${{ $product['price'] }}</p>
+            </a>
+            @endforeach
+
+        @endif
+
 
     </section>
 
